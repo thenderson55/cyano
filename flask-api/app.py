0bf449efe9c35c8eff29f99cf111cf7dd1598516
Thomas Henderson <tbhenderson55@yahoo.com>
2019-08-06 17:57:25 +0900

    cors added for development env

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+
 import os
 
 # Init app
 app = Flask(__name__)
+CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
